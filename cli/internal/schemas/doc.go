13c80e2dd457d4c9d500c713eb65931cd46bf935
Joshua Gilman <joshuagilman@gmail.com>
2025-10-07 21:11:40 -0700

    chore: fresh start

--- cli/internal/schemas/doc.go
+++
@@ -1,5 +0,0 @@
-// Package schemas contains the data structures and schemas used by Forge AI,
-// including project and task definitions generated from CUE schemas.
-package schemas
-
-//go:generate cue exp gengotypes
